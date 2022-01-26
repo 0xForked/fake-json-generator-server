db3eab06d8076757c5d1a699b064a5536ff4c0fa
A. A. Sumitro <hello@aasumitro.id>
2022-01-26 13:17:49 +0800

    impl apply filter

--- src/features/fake/list.mjs
+++ src/features/fake/list.mjs
@@ -2,16 +2,39 @@ import path from 'path'
 import fs from 'fs'
 
 export default function(req, res) {
-    const urlParam = req.params.data
+   let data = loadData(req, res)
 
+    if (req.query.id || req.query.name) {
+        applyFilter(req, res, data)
+    }
+
+    res.send({ code: 200, status: "OK", data: data })
+}
+
+function loadData(req, res) {
+    const urlParam = req.params.data
     const dataPath = path.join(__dirname, 'mock', 'data', `${urlParam}.json`)
 
     if(!fs.existsSync(dataPath)) {
-        res.status(404).send({ code: 404, status: "NOT_FOUND" })
+        return res.status(404).send({ code: 404, status: "NOT_FOUND" })
     }
 
     const dataFile = fs.readFileSync(dataPath, 'utf8')
-    const data = JSON.parse(dataFile)
+    return JSON.parse(dataFile)
+}
 
-    res.send({ code: 200, status: "OK", data: data })
+function applyFilter(req, res, data) {
+    for (let key in data) {
+        if (data.hasOwnProperty(key) && req.query.name) {
+            if (data[key]["name"].toLowerCase() === req.query.name.toLowerCase()) {
+                return res.send({
+                    code: 200,
+                    status: "OK",
+                    data: data[key]
+                })
+            }
+        }
+    }
+
+    return res.status(404).send({ code: 404, status: "NOT_FOUND" })
 }
